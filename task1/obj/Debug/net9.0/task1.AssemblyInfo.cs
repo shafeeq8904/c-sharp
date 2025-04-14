3fd7821cc822a0cc43635e9dea8a43a38a1dc1f6
shafeeq8904 <shafeeq8904@gmail.com>
2025-04-14 12:36:18 +0530

    completed task-3

--- task1/obj/Debug/net9.0/task1.AssemblyInfo.cs
+++ task1/obj/Debug/net9.0/task1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e132c94ca768e5f96eb8074cf2db2453c35ce1c")]
 [assembly: System.Reflection.AssemblyProductAttribute("task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ task3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = new List<string>();
+            names.Add("shafeeq");
+            names.Add("karthik");
+            names.Add("srinivas");;
+
+            Console.WriteLine("names in the list: and convert to upper case");
+            foreach (string name in names)
+            {
+                Console.WriteLine(name.ToUpper());
+            }
+
+            Console.WriteLine("names in the list: and convert to lower case");
+            foreach (string name in names)
+            {
+                Console.WriteLine(name.ToLower());
+            }
+
+            names.Remove("srinivas");
+
+            Console.WriteLine("remove the name from the  list:");
+
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
